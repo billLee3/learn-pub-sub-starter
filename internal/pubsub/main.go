cf95702ae991c922b7d697d9b91f63ae9f3b0e69
billLee3 <bill.lee.third@gmail.com>
2025-11-18 14:43:33 -0600

    Setting up RabbitMq channel

---
+++ internal/pubsub/main.go
@@ -0,0 +1,18 @@
+package pubsub
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
+	bytes, err := json.Marshal(val)
+	if err != nil {
+		return fmt.Errorf("unable to convert value into json bytes: %v", err)
+	}
+	ch.PublishWithContext(context.Background(), exchange, key, false, false, amqp.Publishing{ContentType: "application/json", Body: bytes})
+	return nil
+}
